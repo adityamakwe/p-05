50b37e47b23aa35813e89ebcad2aafbf0daf4918
adityamakwe <35184646+adityamakwe@users.noreply.github.com>
2026-01-09 13:50:20 +0530

    Update app-routing.module.ts

--- sop-django-ui/src/app/app-routing.module.ts
+++ sop-django-ui/src/app/app-routing.module.ts
@@ -166,9 +166,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  
-  imports: [RouterModule.forRoot(routes)],
-  
+   
   imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
